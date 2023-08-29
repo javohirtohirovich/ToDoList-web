c46607b7d911e9fb578c6027e91cb94d4d392cee
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2023-08-29 22:41:58 +0500

    integrated tailwind and flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+
+}
+
